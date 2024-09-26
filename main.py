ab86b1c30e812de16f0beef46b78405da9b14715
Flavi Patel <divyapatel8166@gmail.com>
2024-09-26 19:06:16 +0530

    created an activity on operations on sets

---
+++ main.py
@@ -0,0 +1,11 @@
+set1 = {1, 4, 8, 5, 6, 6,}
+print(set1)
+
+set1.add(3)
+print(set1)
+
+setA = {1, 3, 5, 7}
+setB = {5, 6, 4, 7}
+
+print("Differences:", setA.difference(setB))
+print("Symmetric differences", setA.symmetric_difference(setB))
